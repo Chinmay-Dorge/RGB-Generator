e10026bf3846bdfeeeb5655c235501e83f5099c0
chinmay <lcs2020022@iiitl.ac.in>
2021-09-04 00:28:27 +0530

    Added Random feature

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function rgbGenerate(){
-    var r = document.getElementById("redValue").value;
-    var g = document.getElementById("greenValue").value;
-    var b = document.getElementById("blueValue").value;
+    let r = document.getElementById("redValue").value;
+    let g = document.getElementById("greenValue").value;
+    let b = document.getElementById("blueValue").value;
     
-    var color = "rgb(" + r + "," + g + "," + b + ")";
+    let color = "rgb(" + r + "," + g + "," + b + ")";
+
+    document.getElementById("output").style.backgroundColor = color;
+    document.getElementById("result").value = color;
+}
+
+function generateRandom(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+
+    let color = "rgb(" + r + "," + g + "," + b + ")";
 
     document.getElementById("output").style.backgroundColor = color;
     document.getElementById("result").value = color;
@@ -12,3 +23,4 @@ function rgbGenerate(){
 document.getElementById("redValue").addEventListener("input",rgbGenerate);
 document.getElementById("greenValue").addEventListener("input",rgbGenerate);
 document.getElementById("blueValue").addEventListener("input",rgbGenerate);
+document.getElementById("random").addEventListener("click",generateRandom);
